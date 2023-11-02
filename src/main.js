af80618e86c6d94096f727f7791fc597c3e699a6
Abdullah Khan <abdullahkhanwork@gmail.com>
2023-11-02 22:00:36 +0500

    add quasar

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { Quasar } from 'quasar'
+import quasarUserOptions from './quasar-user-options'
 
-const app = createApp(App)
+const app = createApp(App).use(Quasar, quasarUserOptions)
 
 app.use(router)
 
